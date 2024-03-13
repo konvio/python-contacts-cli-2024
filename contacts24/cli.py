0b5db4c190cd7a4c3cf3fa46f9d7019984681292
Ihor Nikolskyi <ihor.nikolskyi@shelf.io>
2024-03-13 12:09:44 +0200

    refactor: add command routing within cli loop

--- contacts24/cli.py
+++ contacts24/cli.py
@@ -13,33 +13,43 @@ from .contacts import (
 )
 from .errors import HELP_ERROR_MESSAGE
 
+from functools import partial
+
+
+contacts = load_contacts_book()
+
+commands = {
+    "hello": lambda x: "How can I help you?",
+    "help": lambda x: f"Available commands: {HELP_ERROR_MESSAGE}",
+    "add": partial(add_contact, contacts=contacts),
+    "change-phone": partial(change_contact, contacts=contacts),
+    "change-email": lambda x: "In development",
+    "change-address": lambda x: "In development",
+    "phone": partial(get_contact_phone, contacts=contacts),
+    "add-birthday": partial(add_birthday, contacts=contacts),
+    "show-birthday": partial(get_contact_birthday, contacts=contacts),
+    "birthdays": partial(get_upcoming_birthdays, contacts=contacts),
+    "all-contacts": partial(get_all_contacts, contacts=contacts),
+    "add-note": lambda x: "In development",
+    "change-note": lambda x: "In development",
+    "find-note": lambda x: "In development",
+    "get-all-notes": lambda x: "In development",
+}
+
 
 def start():
-    contacts = load_contacts_book()
+    print("Welcome to the assistant bot! Type 'help' to see available commands.")
     try:
         while True:
             user_input = input("Enter a command: ")
-            command, *args = parse_input(user_input)
-            if command in ["close", "exit"]:
-                contacts.save_to_file()
-                print("Contact book is saved, Good bye!")
+            command, args = parse_input(user_input)
+
+            if command == "exit":
+                print("Good bye!")
                 break
-            if command in ["hello", "hi"]:
-                print("How can I help you?")
-            elif command == "add":
-                print(add_contact(args, contacts))
-            elif command == "add-birthday":
-                print(add_birthday(args, contacts))
-            elif command == "change":
-                print(change_contact(args, contacts))
-            elif command == "phone":
-                print(get_contact_phone(args, contacts))
-            elif command == "show-birthday":
-                print(get_contact_birthday(args, contacts))
-            elif command == "birthdays":
-                print(get_upcoming_birthdays(contacts))
-            elif command == "all":
-                print(get_all_contacts(contacts))
+
+            if command in commands:
+                print(commands[command](args))
             else:
                 print(f"Unknown command '{command}', please try again.\n{HELP_ERROR_MESSAGE}")
     except Exception as e:
