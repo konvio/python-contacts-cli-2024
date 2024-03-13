c14bafd9383ff11bdff1670f63bb3d16d0278fa9
Roman Savchuk <roma1995rff2013@gmail.com>
2024-03-13 18:10:01 +0200

    Add notes saving and serialization

---
+++ contacts24/models/note.py
@@ -0,0 +1,8 @@
+
+from contacts24.models.record import Field
+
+# Temporary contract
+class Note(Field):
+    
+    def add_text(text: str, id: int):
+        pass
--- contacts24/models/notes.py
+++ contacts24/models/notes.py
@@ -1,3 +1,11 @@
+from contacts24.models.record import Field
+from contacts24.models.note import Note
+
+# Temporary contract
+class Notes(UserDict):
+    def add_note(self, note: Note) -> None:
+        pass
+
 def get_notes():
     pass
 
