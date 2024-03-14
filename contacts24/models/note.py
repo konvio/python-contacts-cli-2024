ef442e68b5e6476be4612889cd17c26a5499305d
Shulga Sergii <shulgapost@gmail.com>
2024-03-14 22:56:05 +0100

    Added new classes and changed some note functions

--- contacts24/models/note.py
+++ contacts24/models/note.py
@@ -1,9 +1,41 @@
 
 from contacts24.models.record import Field
 
-# Temporary contract
-class Note(Field):
-    
-    def add_text(self, text: str, id: int):
-        # To complete by Sergii Shulga
-        pass
+
+class Id(Field):
+    pass 
+
+
+class Title(Field):
+    pass 
+
+
+class Text(Field):
+    pass 
+
+
+class Note():
+    def __init__(self, id: int, title: str, text=""):
+        self.id = Id(id)
+        self.title = Title(title)
+        self.text = Text(text)
+        self.tags = []
+
+    def add_text(self, id: int, text: str):
+        """Add text to the note with the given id."""
+        if id in self.text:
+            self.text += "\n" + text  # Append text to existing text
+        else:
+            self.text = text
+
+    def get_text(self, id: int) -> str:
+        """Get the text of the note with the given id."""
+        return self.text.get(id, "")
+
+    def add_tag(self, tag: str):
+        """Add a tag to note."""
+        self.tags.append(tag)
+
+    def search_by_tag(self, tag: str) -> bool:
+        """Check if the note contains the specified tag."""
+        return tag in self.tags
--- contacts24/models/notes.py
+++ contacts24/models/notes.py
@@ -3,27 +3,37 @@
 
 from collections import UserDict
 from contacts24.errors import NonExistingNote
+from contacts24.models.note import Note
+from contacts24.models.record import Field
 
 
 
 class Notes(UserDict):
+    
+
     """This class provides functionality for working with notes"""
     def add_note(self, title, text):
         """Adds a new note to the notebook."""
-        max_index = max(self.data.keys()) if self.data else 0
-        note_id = max_index + 1  # Generates a unique identifier for the note
-        self.data[note_id] = {'note_id': note_id, 'title': title, 'text': text}
+        try:
+            max_index = max(self.data.keys())
+            note_id = max_index + 1  # Generates a unique identifier for the note
+        except ValueError:
+            note_id = 1
+    
+        self.data[note_id] = Note(note_id, title, text)
+
+    
 
     def change_note(self, key, new_text):
         """Changes the text of an existing note by its key."""
         if key in self.data:
-            self.data[key]['text'] = new_text
+            self.data[key].text = new_text
         else:
             raise NonExistingNote()
 
     def get_notes(self):
         """Returns all notes from the notebook."""
-        return self.data.values()
+        return list(self.data.values())
 
     def delete_note(self, key):
         """Deletes a note by its key."""
@@ -36,7 +46,7 @@ class Notes(UserDict):
         """Searches for notes by a text query."""
         found_notes = []
         for note_id, note_data in self.data.items():
-            if search_query in note_data['text']:
+            if search_query in note_data.text:
                 found_notes.append((note_id, note_data))
         return found_notes 
-    
+        
