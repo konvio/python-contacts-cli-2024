6cbf074c142741e62010d37eef607de0c5ceb29f
Ihor Nikolskyi <ihor.nikolskyi@shelf.io>
2024-03-14 16:51:43 +0200

    refactor: move get_help to cli_commands

--- contacts24/cli.py
+++ contacts24/cli.py
@@ -6,7 +6,7 @@ from colorama import Fore, init
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.styles import Style
-from .cli_commands import Command
+from .cli_commands import Command, get_help
 
 from .config import PROMPT_MESSAGE, PROMPT_STYLE
 from .contacts import (
@@ -49,37 +49,6 @@ commands = {
 }
 
 
-def get_help() -> str:
-    """Generate help based on the available list of commands
-
-    Returns:
-        str: Help table
-    """
-    message = "\n"
-    
-    padding_command = len("Command")
-    padding_description = len("Description")
-    padding_format = len("Format")
-    
-    for name, command in commands.items():
-        if command.is_hidden:
-            continue
-        padding_command = max([padding_command, len(name)])
-        padding_description = max([padding_description, len(command.description)])
-        padding_format = max([padding_format, len(command.format)])
-    
-    message += (("| {0:-^%s} " % padding_command ) + ("| {0:-^%s} |" % padding_description) + (" {0:-^%s} |\n" % padding_format)).format("-")
-    message += (("| {0:^%s} " % padding_command ) + ("| {1:^%s} |" % padding_description) + (" {2:^%s} |\n" % padding_format)).format("Command", "Description", "Format")
-    message += (("| {0:-^%s} " % padding_command ) + ("| {0:-^%s} |" % padding_description) + (" {0:-^%s} |\n" % padding_format)).format("-")
-    
-    for name, command in commands.items():
-        if command.is_hidden:
-            continue
-        message += (("| {0:<%s} " % padding_command ) + ("| {1:<%s} |" % padding_description) + (" {2:<%s} |\n" % padding_format)).format(command.name, command.description, command.format)
-    message += (("| {0:-^%s} " % padding_command ) + ("| {0:-^%s} |" % padding_description) + (" {0:-^%s} |\n" % padding_format)).format("-")
-    
-    return message
-
 def start():
     print(Fore.LIGHTGREEN_EX + "Welcome to the Contants24!\n" + Fore.WHITE + "Type 'help' to see available commands.\n")
     command_completer = WordCompleter(list(commands.keys()))
@@ -97,7 +66,7 @@ def start():
             if command in commands:
                 print(Fore.WHITE + str(commands[command]()(args)))
             else:
-                help_text = get_help()
+                help_text = get_help(commands)
                 print(Fore.RED + f"Unknown command '{command}', please try again.\n{help_text}")
         except Exception as e:
             print(Fore.RED + f"Unexpected error occured {e}")
