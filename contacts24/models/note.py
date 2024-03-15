d02e2f53dd246f0dcabe3e451c9bf9aa20a48fbe
Roman Savchuk <roma1995rff2013@gmail.com>
2024-03-15 18:58:37 +0200

    Fixes in notes

--- contacts24/models/note.py
+++ contacts24/models/note.py
@@ -4,44 +4,33 @@ from contacts24.models.record import Field
 class Id(Field):
     pass 
 
-
-class Title(Field):
-    pass 
-
-
 class Text(Field):
     pass 
-
 
 class Note():
     def __init__(self, id: int, title: str, text=""):
         self.id = Id(id)
-        self.title = Title(title)
         self.text = Text(text)
         self.tags = []
 
-    def add_text(self, id: int, text: str):
-        """Add text to the note with the given id."""
-        if id in self.text:
-            self.text += "\n" + text  # Append text to existing text
-        else:
-            self.text = text
-
     def get_text(self, id: int) -> str:
         """Get the text of the note with the given id."""
+        
         return self.text.get(id, "")
 
     def add_tag(self, tag: str):
         """Add a tag to note."""
+        
         self.tags.append(tag)
 
-    def contains_tag(self, tag: str) -> bool:
+    def contains_tag(self, tag: str) -> bool:        
         """Check if the note contains the specified tag."""
+        
         return tag in self.tags
     
     def __str__(self):
         return (f"Note:\n"
                 f"  Id: {self.id}\n"
-                f"  Title: {self.title}\n"
-                f"  Text: {self.text}\n"
-                f"  Tags: {', '.join(self.tags)}")
+                f"  Tags: {', '.join(self.tags)}\n\n"
+                f"  Text: {self.text}")
+
--- contacts24/models/notes.py
+++ contacts24/models/notes.py
@@ -4,41 +4,47 @@ from contacts24.models.note import Note
 
 
 class Notes(UserDict):
-
     """This class provides functionality for working with notes"""
-    def add_note(self, title, text):
+    
+    def add_note(self, text: str):
         """Adds a new note to the notebook."""
+        
         try:
             max_index = max(self.data.keys())
             note_id = max_index + 1  # Generates a unique identifier for the note
         except ValueError:
             note_id = 1
     
-        self.data[note_id] = Note(note_id, title, text)
+        self.data[note_id] = Note(note_id, text)
 
-    def change_note(self, key, new_text):
-        """Changes the text of an existing note by its key."""
-        if key in self.data:
-            self.data[key].text = new_text
-        else:
+    def change_note(self, id: in, new_text: str):
+        """Changes the text of an existing note by its id."""
+        
+        if not id in self.data:
             raise NonExistingNote()
+        
+        self.data[id].text = new_text
 
     def get_notes(self):
         """Returns all notes from the notebook."""
+        
         return list(self.data.values())
 
-    def delete_note(self, key):
-        """Deletes a note by its key."""
-        if key in self.data:
-            del self.data[key]
-        else:
+    def delete_note(self, id: int):
+        """Deletes a note by its id."""
+        
+        if not id in self.data:
             raise NonExistingNote()
+        
+        del self.data[id]
 
-    def search_text(self, search_query):
+    def search_text(self, search_query: str) -> list[Note]:
         """Searches for notes by a text query."""
+        
         found_notes = []
+        
         for note_id, note_data in self.data.items():
             if search_query in note_data.text:
-                found_notes.append((note_id, note_data))
+                found_notes.append(note_data)
         return found_notes 
         
